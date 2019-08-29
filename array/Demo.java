409ae4924c0dea06c61ec2c4b2dd3f667e9a4251
zhuxinkai <zhuxinkai@126.com>
2019-08-29 23:40:36 +0800

    array
    2019/08/29

---
+++ array/Demo.java
@@ -0,0 +1,30 @@
+class Demo{
+	
+	
+	
+
+
+
+public static void main (String[] args) {
+	
+	int [] array = {1,2,3,234,12312,42534,1212,424};
+   Arrayprint(array);
+   
+
+	
+}
+	public static void Arrayprint(int[] h)   {
+   
+	for (int i=0; i<h.length; i++)
+	 {
+	 	
+	 	System.out.print(h[i]+"\t");
+	 	
+	 }
+	
+}
+
+
+
+
+   }
