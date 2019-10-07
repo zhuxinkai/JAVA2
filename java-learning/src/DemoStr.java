88c5b0fb538c21fef81adcb2e53d35d82d54dce1
zhuxinkai <zhuxinkai@126.com>
2019-10-07 22:12:03 +0800

    2019-10-07-idea-project with str

---
+++ java-learning/src/DemoStr.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class DemoStr {
+    public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+   Demo jj = new Demo();
+   jj.printNum(str);
+
+    }
+}
+
+
+
+
+
+class Demo{
+
+    public static void printNum(String str){
+        int Anum = 0;
+        int anum = 0;
+        int num = 0;
+
+        for(int i = 0;i< str.length();i++){
+            char c = str.charAt(i);
+            if(c >= 'A' && c <= 'Z'){
+                Anum ++;
+
+            }else if(c >= 'a' && c <= 'z'){
+                anum ++;
+
+
+            }else if(c >= '0' && c <= '9'){
+                num ++;
+            }
+
+
+
+        }
+
+        System.out.println("字符串中大写字母的个数是"+Anum);
+        System.out.println("字符串中小写字母的个数是"+anum);
+        System.out.println("字符串中数字的个数是"+num);
+    }
+
+
+
+}
