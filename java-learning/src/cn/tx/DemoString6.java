60ad71d64c80449a3167a9de4968ea03e4f82757
zhuxinkai <zhuxinkai@126.com>
2019-10-08 23:39:05 +0800

    2019-10-08-split

--- java-learning/src/cn/tx/DemoString6.java
+++ java-learning/src/cn/tx/DemoString6.java
@@ -3,7 +3,28 @@ package cn.tx;
 public class DemoString6 {
     public static void main(String [] args){
 
-        System.out.println("xhdi ");
+       String str1 = "120446461316:kexin:20:1|430181982510164:liusha:35:2|43016464674936464:zhuyonghan:9:1";
+
 
     }
+
+
+
+
+}
+
+
+class Person{
+
+    String id;
+    char gender;
+    int age;
+    String name;
+
+    public Person(String id,char gender,int age,String name){
+
+
+    }
+
+
 }
