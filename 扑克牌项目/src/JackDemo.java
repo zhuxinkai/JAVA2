346052280e78a312a1a1ac85116942a389788c1b
zhuxinkai <zhuxinkai@126.com>
2019-10-30 23:23:43 +0800

    扑克牌-项目

---
+++ 扑克牌项目/src/JackDemo.java
@@ -0,0 +1,16 @@
+public class JackDemo {
+    public static void main (String [] args){
+        for (int j = 0 ; j < 3; j++){
+            for (int i = 0; i<14; i++){
+
+
+
+            }
+
+
+        }
+
+
+
+    }
+}
